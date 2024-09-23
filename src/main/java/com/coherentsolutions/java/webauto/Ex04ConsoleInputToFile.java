66961d5eb917f645fd705e44f052d628c287b4b2
iWaraxe <igor.waraxe@gmail.com>
2024-09-23 13:06:03 +0200

    Try with resources update

--- src/main/java/com/coherentsolutions/java/webauto/Ex04ConsoleInputToFile.java
+++ src/main/java/com/coherentsolutions/java/webauto/Ex04ConsoleInputToFile.java
@@ -5,19 +5,13 @@ import java.io.*;
 public class Ex04ConsoleInputToFile {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        FileWriter fileOut = null;
 
-        try {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileWriter fileOut = new FileWriter("copied_file.txt");) {
+
             System.out.println("Enter text to write to file:");
             String someStr = reader.readLine();
-            fileOut = new FileWriter("copied_file.txt");
             fileOut.write(someStr);
-        } finally {
-            reader.close();
-            if (fileOut != null) {
-                fileOut.close();
-            }
         }
     }
 }
